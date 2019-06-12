9157ae3e0555ca7f0bffe0884724a1435bfdb6fb
Eric <github@esellors.com>
2019-06-12 14:13:50 -0500

    initial setup and step 2 completed

---
+++ src/redux/budgetReducer.js
@@ -0,0 +1,16 @@
+
+
+const initialState = {
+   purchases: [],
+   budgetLimit: null,
+   loading: false
+}
+
+export default function budgetReducer(state = initialState, action) {
+   const {type, payload} = action;
+
+   return state;
+   // switch(type) {
+   //    default: return state;
+   // }
+}
